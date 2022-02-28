8a0ea2a56ddecf3a8a88407f8e63114aa5b448b6
JazzAfk <77739604+Jazzafk@users.noreply.github.com>
2022-02-28 13:13:14 -0500

    BARCODE

---
+++ Barcode/working/working.js
@@ -0,0 +1,23 @@
+const constraints = window.constraints = {
+  audio: false,
+  video: true,
+  facingMode: 'environment',
+};
+
+function handleSuccess(stream) {
+  const video = document.getElementById('video');
+  window.stream = stream;
+  video.srcObject = stream;
+}
+
+async function init(e) {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    handleSuccess(stream);
+  } catch (e) {
+    handleError(e);
+  }
+}
+
+document.getElementById('showVideo').addEventListener('click', e => init(e));
+
